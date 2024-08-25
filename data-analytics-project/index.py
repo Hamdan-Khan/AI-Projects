1de405b0ae43d9f240a3cbd83382130af100a212
Hamdan-Khan <hamdankhan212@gmail.com>
2024-08-25 22:26:09 +0500

    column based approach used

--- data-analytics-project/index.py
+++ data-analytics-project/index.py
@@ -4,41 +4,28 @@ from pathlib import Path
 import argparse
 
 def read_csv(filename):
-    columns_to_keep = {'food', 'Protein', 'Caloric Value', 'Fat', 'Carbohydrates'}
-
     with open(filename, 'r') as csvfile:
-        csvreader = csv.DictReader(csvfile)
-        data_list = list(csvreader)
-        filtered = []
-        for row in data_list:
-            filtered_row = {k: v for k, v in row.items() if any(k.startswith(prefix) for prefix in columns_to_keep)}
-            filtered.append(filtered_row)
-        total_num = len(data_list) - 1
+        reader = list(csv.reader(csvfile))[1:]
+        proteins = [float(row[10]) for row in reader]
+        fats = [float(row[4]) for row in reader]
+        carbs = [float(row[8]) for row in reader]
+        cals = [float(row[3]) for row in reader]
+        data_dict = {"proteins":proteins,"fats":fats,"carbs":carbs,"cals":cals}
+        total_num = len(proteins) - 1
 
     print("Total no. of entries:", total_num)
 
-    n = int(input("Enter the number of entries you want to see: "))
-    
-    if n < 1 or n > total_num:
-        print("Out of range")
-    else:
-        print(f'\nFirst {n} rows are:\n')
-        for data in filtered[:n]:
-            print(data)
-
-    return filtered, total_num
+    return data_dict, total_num
 
 # fats, protein, carbs, calories
-def analyze_data(data_list,size):
+def analyze_data(data_dict,size):
     print("\n\n\n Analysis of Data:   \n\n\n")
-    sums = {"calories":0,"protein":0,"carbs":0,"fats":0}
-    for data in data_list[:5]:
-        sums["protein"] += float(data["Protein"])
-        sums["calories"] += float(data["Caloric Value"])
-        sums["fats"] += float(data["Fat"])
-        sums["carbs"] += float(data["Carbohydrates"])
-    print("Sums: ",sums)
-    
+
+    print("Mean of proteins (in g):  ", statistics.mean(data_dict["proteins"]))
+    print("Mean of fats (in g):  ", statistics.mean(data_dict["fats"]))
+    print("Mean of carbs (in g):  ", statistics.mean(data_dict["carbs"]))
+    print("Mean of cals (in g):  ", statistics.mean(data_dict["cals"]))
+
 
 
 # def filter_data(data, column, value):
@@ -59,8 +46,8 @@ def main():
         print("The target directory doesn't exist")
         raise SystemExit(1)
 
-    data_list, total_num = read_csv(file_path)
-    analyze_data(data_list,total_num)
+    data_dict, total_num = read_csv(file_path)
+    analyze_data(data_dict,total_num)
     
 
 if __name__ == "__main__":
