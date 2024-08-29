79b27664607abfaeadeb6c9f4d409734214ba2ad
Hamdan-Khan <hamdankhan212@gmail.com>
2024-08-29 23:39:06 +0500

    load_dataset implemented

--- data-preprocessing-project/index.py
+++ data-preprocessing-project/index.py
@@ -3,30 +3,47 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
 def load_data(file_path):
-    # Load the dataset using pandas
+    data = pd.read_csv(
+        file_path,
+        usecols=[
+            "Unnamed: 0",
+            "Title",
+            "Type",
+            "BodyPart",
+            "Equipment",
+            "Level",
+            "Rating",
+        ],
+        nrows=1000,
+    )
+    return data
 
-def explore_data(df):
-    # Display basic information about the dataset
-    # Show summary statistics
 
-def handle_missing_values(df):
-    # Identify and handle missing values
+# def explore_data(df):
+#     # Display basic information about the dataset
+#     # Show summary statistics
 
-def convert_data_types(df):
-    # Convert data types if necessary
+# def handle_missing_values(df):
+#     # Identify and handle missing values
 
-def remove_duplicates(df):
-    # Check for and remove duplicate entries
+# def convert_data_types(df):
+#     # Convert data types if necessary
 
-def feature_engineering(df):
-    # Create new features or transform existing ones
+# def remove_duplicates(df):
+#     # Check for and remove duplicate entries
 
-def visualize_data(df):
-    # Create visualizations to better understand the data
+# def feature_engineering(df):
+#     # Create new features or transform existing ones
+
+# def visualize_data(df):
+#     # Create visualizations to better understand the data
+
 
 def main():
-    # Implement the main workflow
+    load_data("megaGymDataset.csv")
+
 
 if __name__ == "__main__":
     main()
