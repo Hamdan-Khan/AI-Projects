bb52281723eae5b48fe5eccac2c8db54856304e7
Hamdan-Khan <hamdankhan212@gmail.com>
2024-09-01 20:21:48 +0500

    preprocessed

--- data-visualization-project/index.py
+++ data-visualization-project/index.py
@@ -5,25 +5,28 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 
-def load_data(file_path):
-    # Load the dataset
+def load_data(file_path) -> pd.DataFrame:
     df = pd.read_csv(file_path)
     return df
 
 
-# def preprocess_data(df):
-#     # Handle missing values, data types, etc.
-#     return df
+def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
+    print(df.dtypes)
+    print(df.isnull().sum())
+    return df
 
-# def create_static_visualizations(df):
-#     # Create various static visualizations using Matplotlib and Seaborn
+
+# def create_static_visualizations(df : pd.DataFrame):
+#     sns.set_style("darkgrid")
+
 
 # def create_interactive_visualizations(df):
 #     # Create interactive visualizations using Plotly
 
 
 def main():
-    df = load_data("world_happiness_report.csv")
+    df = load_data("2019.csv")
+    df = preprocess_data(df)
 
 
 if __name__ == "__main__":
