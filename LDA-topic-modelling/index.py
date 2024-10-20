c7d4924709c2f09536709bdf98363dac1c564d13
Hamdan-Khan <hamdankhan212@gmail.com>
2024-10-20 21:27:25 +0500

    download path fixed

--- LDA-topic-modelling/index.py
+++ LDA-topic-modelling/index.py
@@ -1,14 +1,14 @@
 import nltk
 import os
 
-nltk.data.path.append("\\tmp")
+nltk.data.path.append("./tmp")
 
 
 def download_nltk_data():
-    if not os.path.exists("\\tmp/nltk_data"):
-        nltk.download("punkt", download_dir="\\tmp")
-        nltk.download("stopwords", download_dir="\\tmp")
-        nltk.download("punkt_tab", download_dir="\\tmp")
+    if not os.path.exists("./tmp/nltk_data"):
+        nltk.download("punkt", download_dir="./tmp")
+        nltk.download("stopwords", download_dir="./tmp")
+        nltk.download("punkt_tab", download_dir="./tmp")
 
 
 def main():
