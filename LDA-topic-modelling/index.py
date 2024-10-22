f7ada94b931ed29eaad33e0fba9d8e24b1621811
Hamdan-Khan <hamdankhan212@gmail.com>
2024-10-22 14:10:39 +0500

    path condition fixed

--- LDA-topic-modelling/index.py
+++ LDA-topic-modelling/index.py
@@ -5,8 +5,10 @@ nltk.data.path.append("./tmp")
 
 
 def download_nltk_data():
-    if not os.path.exists("./tmp/corpora/stopwords") or not os.path.exists(
-        "./tmp/tokenizers/punkt"
+    if (
+        not os.path.exists("./tmp/corpora/stopwords")
+        or not os.path.exists("./tmp/tokenizers/punkt")
+        or not os.path.exists("./tmp/tokenizers/punkt_tab")
     ):
         nltk.download("punkt", download_dir="./tmp")
         nltk.download("stopwords", download_dir="./tmp")
